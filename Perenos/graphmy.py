fbf49a05c006b63fc8eda69d1f769fde06f5e8f7
MMiknich <MMiknich@gmail.com>
2020-05-17 14:14:11 +0300

    Added 4th task

--- Perenos/graphmy.py
+++ Perenos/graphmy.py
@@ -2,14 +2,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.array(pd.read_csv("out_file.csv", sep=';')['time'])
+data = np.array(pd.read_csv("per.csv", sep=';')['time'])
+print(data)
+a = np.ones((data.size,)) * data[0]
+print(a, data)
+data = a/data
+
+plt.plot(np.linspace(1, 4, 4), data, "r-o", label='long')
+
+data = np.array(pd.read_csv("ter.csv", sep=';')['time'])
 
 a = np.ones((data.size,)) * data[0]
 print(a, data)
 data = a/data
 
-plt.plot(np.linspace(1, 16, 16), data, "r-o")
+plt.plot(np.linspace(1, 16, 16), data, "b-o", label='short')
+
 plt.xlabel("Number of threads")
 plt.ylabel("Acceleration")
+plt.legend()
 plt.grid()
 plt.show()
